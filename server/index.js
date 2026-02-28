390e00d8149515be48ad80ae52c6405cc41f4723
Craig Chang <craigchang@gmail.com>
2026-02-27 23:11:24 -0800

    fix npm vulnerabilities

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const materialsJson = require('./rest/materials');
 const recipesJson = require('./rest/recipes');
